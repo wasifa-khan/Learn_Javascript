4eb9df4f23532e6717dc87fdf5bc7378b72e14cf
wasifa-khan <83424343+wasifa-khan@users.noreply.github.com>
2024-05-01 16:09:08 +0500

    uploaded

---
+++ Learn_Javascript/JavascriptClass01/script.js
@@ -0,0 +1,31 @@
+// console.log("Hello World");
+// console.log("Wasifa Khan");
+// document.write("Hello Javascript");
+
+// alert("This is not working");
+
+// var first_name;  //declair
+// first_name = 'wasifa';
+// var first_name = 'wasifa';
+
+// let FirstName = 'khan';
+// console.log(first_name);
+// console.log(FirstName);
+
+
+// var text = "It is invented in 1845";
+// console.log(text);
+
+// var name = "Wasifa Khan";
+// var email = "wasifaasadk@gmail.com";
+// var dateOfBirth = 2002;
+// console.log(name + " " + email + " " + dateOfBirth);
+// alert(name + "  " + email + "  " + dateOfBirth);
+// console.log(email);
+// console.log(dateOfBirth);
+
+
+let a = 12;
+let b= 16;
+let result = a + b;
+console.log("The sum of two number is: " + (a + b));
